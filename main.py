5d781c9ae45496b471b952c90331e20288666c14
Aplecy <njdarby19@gmail.com>
2025-03-02 15:34:13 -0500

    Started Program, made inital values

---
+++ main.py
@@ -0,0 +1,17 @@
+"""
+This program calculates prices for custom house signs.
+"""
+
+# Declare and initialize variables here.
+# Charge for this sign.
+charge = 0.00
+# Number of characters.
+numChars = 8
+# Color of characters.
+color = "gold"
+# Type of wood.
+woodType = "oak"
+# Write assignment and if statements here as appropriate.
+
+# Output Charge for this sign.
+print("The charge for this sign is $" + str(charge))
